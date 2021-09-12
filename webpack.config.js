9866eea9e6c76c7bada7efed9708539f06723350
JakeFenley <jfenley6@gmail.com>
2021-09-12 17:20:06 +0100

    simplify dev server/tooling

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
-var path = require('path');
-
+const path = require('path');
 module.exports = {
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
     },
-    compress: true,
-    port: 3000,
-  },
+    resolve: {
+        extensions: ['.ts', '.js', '.tsx']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000,
+    },
 };
